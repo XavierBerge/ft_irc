1de37508069cf068844f87422c93b2bf09c6f16b
Xavier Berger <xavier.berger66@gmail.com>
2024-11-07 21:19:53 +0100

    small fix on mode +o, mode -o added

--- Mode.cpp
+++ Mode.cpp
@@ -6,7 +6,7 @@
 /*   By: xav <xav@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 21:12:32 by xav               #+#    #+#             */
-/*   Updated: 2024/11/07 17:15:13 by xav              ###   ########.fr       */
+/*   Updated: 2024/11/07 21:15:14 by xav              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,14 +73,55 @@ void Server::handleModePlusO(int client_fd, const std::string& command)
         clients[client_fd]->sendToClient("441 " + clients[client_fd]->getNickname() + " " + nickname + " " + channelName + " :They aren't on that channel\r\n");
         return;
     }
-	channel->promoteToOperator(nickname, target_client_fd);
-	std::string response = ":" +  clients[client_fd]->getNickname() + " MODE " + channelName + " +o " + nickname + "\r\n";
-    channel->broadcastMessage(response);  // Broadcast à tout le canal
+	if (!channel->isOperator(nickname))
+	{
+		channel->promoteToOperator(nickname, target_client_fd);
+		std::string response = ":" +  clients[client_fd]->getNickname() + " MODE " + channelName + " +o " + nickname + "\r\n";
+		channel->broadcastMessage(response);  // Broadcast à tout le canal	
+	}
+	else
+		clients[client_fd]->sendToClient(":" + servername + " " + nickname + " " + channelName + " : User is already operator on that channel\r\n");
 }
 
 void Server::handleModeMinusO(int client_fd, const std::string& command) 
 {
-    std::cout << "handleModeMinusO called with command: " << command << " for client_fd: " << client_fd << std::endl;
+    std::istringstream iss(command);
+	std::string mode, channelName, option, nickname;
+	iss >> mode >> channelName >> option >> nickname; 
+
+	if (nickname.empty())
+		clients[client_fd]->sendToClient("461 " + clients[client_fd]->getNickname() + " MODE :Not enough parameters\r\n");
+	
+
+	int target_client_fd = -1;
+    for (std::map<int, Client*>::iterator it = clients.begin(); it != clients.end(); ++it) 
+	{
+        if (it->second->getNickname() == nickname) 
+		{
+            target_client_fd = it->first;
+            break;
+        }
+    }
+	if (target_client_fd == -1) 
+	{
+        clients[client_fd]->sendToClient("401 " + clients[client_fd]->getNickname() + " " + nickname + " :No such nick/channel\r\n");
+        return;
+    }
+	Channel *channel = channels[channelName];
+
+	if (!channel->isClientInChannel(nickname)) 
+	{
+        clients[client_fd]->sendToClient("441 " + clients[client_fd]->getNickname() + " " + nickname + " " + channelName + " :They aren't on that channel\r\n");
+        return;
+    }
+	if(channel->isOperator(nickname))
+	{
+		channel->removeOperator(nickname);
+		std::string response = ":" +  clients[client_fd]->getNickname() + " MODE " + channelName + " -o " + nickname + "\r\n";
+		channel->broadcastMessage(response);
+	}
+	else
+		clients[client_fd]->sendToClient(":" + servername + " " + nickname + " " + channelName + " : User is not operator on that channel\r\n");
 }
 
 void Server::handleModePlusI(int client_fd, const std::string& command) 
